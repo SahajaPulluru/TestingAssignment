d6132bfc1960399e0ae640765edd21abb4cfd223
SahajaPulluru <60398544+SahajaPulluru@users.noreply.github.com>
2022-10-27 07:50:09 +0530

    Add files via upload

---
+++ TestAssignment/TempSeviceXunit.Tests/TempXunitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using System.Text;
+using Xunit.Sdk;
+
+namespace TempService.test
+{
+    public class TempXUnitTest
+    {
+        [Fact]
+        public void TestCelsiusToFahrenheit()
+        {
+            Temp t = new Temp();
+            double actual = t.CelsiusToFahrenheit(30);
+            double expected = 86;
+            Assert.Equal(expected,actual);
+        }
+        [Fact]
+        public void TestFahrenheitToCelsius()
+        {
+            Temp t = new Temp();
+            double actual = t.FahrenheitToCelsius(86);
+            double expected = 30;
+            Assert.Equal(expected, actual);
+
+        }
+
+    }
+}
